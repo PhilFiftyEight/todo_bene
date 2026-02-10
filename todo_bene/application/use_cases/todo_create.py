9921e6d7209cc1eb332c2a296a36a61de4f2b2bb
PhilFiftyEight <phil_fiftyeight@orange.fr>
2026-02-10 23:18:08 +0100

    fix: passage 3.14 > 3.13 error (Exception1, Exception2...)

--- todo_bene/application/use_cases/todo_create.py
+++ todo_bene/application/use_cases/todo_create.py
@@ -23,7 +23,7 @@ class TodoCreateUseCase:
             for fmt in formats:
                 try:
                     return pendulum.from_format(date_str, fmt, tz=tz)
-                except ValueError, pendulum.parsing.exceptions.ParserError:
+                except (ValueError, pendulum.parsing.exceptions.ParserError):
                     continue
             raise ValueError(f"Format de date non supporté : {date_str}")
 
